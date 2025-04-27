ddb838874a9700f4f9031503a6c289d0470cd90c
alexlima331 <202501424988@alunos.estacio.br>
2025-04-27 20:34:18 +0000

    VERSÃO FINAL - JOGO DE XADREZ

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,266 @@
 #include <stdio.h>
+#include <string.h>
 
 // Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+// Trabalho de Alex A. de Lima
+
+//Recursiva para o movimento da torre
+void moveTorre (int casas, int direcao){
+    if (casas>0 && direcao==1){
+        printf("TORRE - DIREITA\n");
+        moveTorre(casas-1, 1);
+    }
+    else if (casas>0 && direcao==2){
+        printf("TORRE - ESQUERDA\n");
+        moveTorre(casas-1, 2);
+    }
+    else if (casas>0 && direcao==3){
+        printf("TORRE - CIMA\n");
+        moveTorre(casas-1, 3);
+    }
+    else if (casas>0 && direcao==4){
+        printf("TORRE - BAIXO\n");
+        moveTorre(casas-1, 4);
+    }
+    else{
+    printf("************************************************************\n\n");
+    }
+}
+
+
+
+//Recursiva para o movimento da rainha
+void moveRainha(int casas, int direcao){
+    if (direcao==1){
+        int i = 0;
+            while(i<casas){
+                printf("RAINHA - DIREITA\n");
+                i++;
+            }
+    }
+    else if (direcao==2){
+        int i = 0;
+            while(i<casas){
+                printf("RAINHA - ESQUERDA\n");
+                i++;
+            }
+    }
+    else if (direcao==3){
+        int i = 0;
+            while(i<casas){
+                printf("RAINHA - CIMA\n");
+                i++;
+            }
+    }
+    else if (direcao==4){
+        int i = 0;
+            while(i<casas){
+                printf("RAINHA - BAIXO\n");
+                i++;
+            }
+    }
+    else if (direcao==5){
+        int i = 0;
+            while(i<casas){
+                printf("RAINHA - DIAGONAL DIREITA\n");
+                i++;
+            }
+    }
+    else if (direcao==6){
+        int i = 0;
+            while(i<casas){
+                printf("RAINHA - DIAGONAL ESQUERDA\n");
+                i++;
+            }
+    }
+
+    printf("************************************************************\n\n");
+}
+
+
+
+
+
+void moveBispo(int casas, int direcao1, int direcao2) {
+    char vertical[30];
+    char horizontal[30];
+
+    if (casas>0){
+        // Define a parte vertical da direção
+        if (direcao1 == 1) {
+            strcpy(vertical, "BISPO - DIAGONAL/CIMA");
+        } else if (direcao1 == 2) {
+            strcpy(vertical, "BISPO - DIAGONAL/BAIXO");
+        } 
+
+        // Define a parte horizontal da direção
+        if (direcao2 == 1) {
+            strcpy(horizontal, "/DIREITA\n");
+        } else if (direcao2 == 2) {
+            strcpy(horizontal, "/ESQUERDA\n");
+        } 
+
+        printf("%s%s", vertical, horizontal);
+
+        // Chamada recursiva com uma casa a menos
+        moveBispo(casas - 1, direcao1, direcao2);
+    }
+    else if(casas == 0){
+        printf("************************************************************\n\n");
+        moveBispo(casas - 1, direcao1, direcao2);
+    }
+}
+
+
+
+
+void moveCavalo (int direcao1, int direcao2) {
+    char vertical[30];
+    char horizontal[30];
+
+         // Define a parte horizontal da direção
+         if (direcao1 == 1) {
+            strcpy(horizontal, "CAVALO - DIREITA");
+        } else if (direcao1 == 2) {
+            strcpy(horizontal, "CAVALO - ESQUERDA");
+        } 
+
+        // Define a parte vertical da direção
+        if (direcao2 == 1) {
+            strcpy(vertical, "- CIMA");
+        } else if (direcao2 == 2) {
+            strcpy(vertical, "- BAIXO");
+        } 
+
+        printf("%s%s%s", horizontal, vertical, vertical);
+        printf("\n************************************************************\n");
+        printf("\n\n\n");
+}
+
+
+/**************************************************************************************************/
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    int i = 0;
+    int movimentoCavalo, movimentoTorre, movimentoRainha, movimentoBispo;
+    int direcaoTorre, direcaoRainha, direcaoBispo, direcaoBispo1, direcaoCavalo, direcaoCavalo1;
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    //Solicita as entradas para o usuário - Jogo mais interativo //
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    /*Para o movimento da torre*/
+    printf("Digite o número que representa o movimento da torre:\n");
+    printf("1 - DIREITA\n");
+    printf("2 - ESQUERDA\n");
+    printf("3 - CIMA\n");
+    printf("4 - BAIXO\n");
+    scanf("%d",&direcaoTorre);
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    printf("Digite a quantidade de casas que a torre irá se mover:");
+    scanf("%d",&movimentoTorre);
+    printf("\n\n");
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    /*Para o movimento da rainha*/
+    printf("Digite o número que representa o movimento da rainha:\n");
+    printf("1 - DIREITA\n");
+    printf("2 - ESQUERDA\n");
+    printf("3 - CIMA\n");
+    printf("4 - BAIXO\n");
+    printf("5 - DIAGONAL DIREITA\n");
+    printf("6 - DIAGONAL ESQUERDA\n");
+    scanf("%d",&direcaoRainha);
+
+
+    printf("Digite a quantidade de casas que a rainha irá se mover:");
+    scanf("%d",&movimentoRainha);
+    printf("\n\n");
+
+
+
+    /*Para o movimento do bispo*/
+    printf("Digite o número que representa o primeiro movimento do bispo:\n");
+    printf("1 - CIMA\n");
+    printf("2 - BAIXO\n");
+    scanf("%d",&direcaoBispo);
+    printf("\n");
+
+
+    printf("Digite o número que representa o primeiro movimento do bispo:\n");
+    printf("1 - ESQUERDA\n");
+    printf("2 - DIREITA\n");
+    scanf("%d",&direcaoBispo1);
+    printf("\n");
+
+
+    printf("Digite a quantidade de casas que o bispo irá se mover:");
+    scanf("%d",&movimentoBispo);
+    printf("\n\n");
+
+
+     /*Para o movimento do cavalo*/
+     printf("Digite o número que representa o primeiro movimento do cavalo:\n");
+     printf("1 - DIREITA\n");
+     printf("2 - ESQUERDA\n");
+     scanf("%d",&direcaoCavalo);
+     printf("\n");
+
+ 
+     printf("Digite o número que representa o primeiro movimento do bispo:\n");
+     printf("1 - CIMA\n");
+     printf("2 - BAIXO\n");
+     scanf("%d",&direcaoCavalo1);
+     printf("\n\n");
+
+
+
+    //Chamada das recursivas
+    //Para torre
+    if (direcaoTorre>0 && direcaoTorre<5){
+        moveTorre(movimentoTorre, direcaoTorre);
+    }else{
+        printf("Você escolheu uma direção de movimento INVÁLIDA para a TORRE.\n\n\n");
+        printf("************************************************************\n\n");
+    }
+
+    //Para rainha
+    if (direcaoRainha>0 && direcaoRainha<7){
+        moveRainha(movimentoRainha, direcaoRainha);
+    }else{
+        printf("Você escolheu uma direção de movimento INVÁLIDA para a RAINHA.\n\n\n");
+        printf("************************************************************\n\n");
+    }
+
+    //Para bispo
+     if (direcaoBispo>0 && direcaoBispo<3 && direcaoBispo1>0 && direcaoBispo1<3){
+        moveBispo(movimentoBispo, direcaoBispo, direcaoBispo1);
+    }else{
+        printf("Você escolheu uma direção de movimento INVÁLIDA para o BISPO.\n\n\n");
+        printf("************************************************************\n\n");
+    }
+
+    //Para cavalo
+    if (direcaoCavalo>0 && direcaoCavalo<3 && direcaoCavalo1>0 && direcaoCavalo1<3){
+        moveCavalo(direcaoCavalo, direcaoCavalo1);
+    }else{
+        printf("Você escolheu uma direção de movimento INVÁLIDA para o CAVALO.\n\n\n");
+    }
+
+
+    //Movimento do cavalo
+    while (movimentoCavalo < 3) {
+        for (int movimentoVertical = 0; movimentoVertical < 3; movimentoVertical++) {
+            if (movimentoVertical % 2 == 0 && movimentoVertical != 0) {
+                printf("CAVALO - DIREITA\n\n");
+                movimentoCavalo++;
+                break;                
+            } else {
+                movimentoCavalo++;
+                printf("CAVALO - CIMA\n");
+            }
+        }
+    }
+   
 
     return 0;
 }
